1310af5e04df7c3b0283d7d77a3bf8ea27430a15
Davit <davit.kv8@gmail.com>
2023-05-30 02:43:00 +0400

    latest changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="compute-strings-data",
+    version="0.0.1",
+    url="https://github.com/davitkv8/challenges/",
+    author="Davit Kvaratskhelia",
+    author_email="davit.kv8@gmail.com",
+    description="Package includes some tools to compute string data in the shortest possible time.",
+    packages=find_packages(),
+    install_requires=[],
+)
