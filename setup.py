c923a208b94780af18b4896ae97577abcb913c86
Davit <davit.kv8@gmail.com>
2023-06-10 16:45:29 +0400

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="compute-strings-data",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/davitkv8/challenges/",
     author="Davit Kvaratskhelia",
     author_email="davit.kv8@gmail.com",
