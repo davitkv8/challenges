a91f34d6d87b52734e1d4b5db2fa60dabf694adb
Davit <davit.kv8@gmail.com>
2023-06-14 17:54:53 +0400

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="compute-strings-data",
-    version="0.1.2",
+    version="0.1.3",
     url="https://github.com/davitkv8/challenges/",
     author="Davit Kvaratskhelia",
     author_email="davit.kv8@gmail.com",
